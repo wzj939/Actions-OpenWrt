ba97a645c28f4217cef323018ce4e9a807db1104
wzj939 <64715818+wzj939@users.noreply.github.com>
2021-07-20 00:03:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Modify default passwork:00000000
+#sed -i 's/root::0:0:99999:7:::/root:$1$/YSxcdBO$bFuXE13KnaJb25YA8b6/1/:18825:0:99999:7:::/g' package/base-files/files/etc/shadow
+# Download luci-app-Poweroff
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/uci-app-poweroff
