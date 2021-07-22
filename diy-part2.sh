6efcd2c86cd766211c99b1844167c5645761d611
wzj939 <64715818+wzj939@users.noreply.github.com>
2021-07-23 01:48:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,7 +15,8 @@
 sed -i '/exit 0/i\uci set network.lan.ipaddr='192.168.1.254'\nuci set network.lan.proto='static'\nuci set network.lan.type='bridge'\nuci set network.lan.ifname='eth0'\nuci set network.lan.netmask='255.255.255.0'\nuci set network.lan.gateway='192.168.1.1'\nuci set network.lan.dns='192.168.1.1'\nuci commit network\n ' package/lean/default-settings/files/zzz-default-settings
 
 # Modify default passwork:00000000
-#sed -i 's/root::0:0:99999:7:::/root:$1$/YSxcdBO$bFuXE13KnaJb25YA8b6/1/:18825:0:99999:7:::/g' package/base-files/files/etc/shadow
+sed -i 's/root::0:0:99999:7:::/root:$1$/YSxcdBO$bFuXE13KnaJb25YA8b6/1/:18825:0:99999:7:::/g' package/base-files/files/etc/shadow
+
 # Download luci-app-Poweroff
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/uci-app-poweroff
 # Download OpenAppFilter
