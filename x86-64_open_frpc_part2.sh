66cd399dc38ca8f278edeea04e57f5781e9d35f0
wzj939 <64715818+wzj939@users.noreply.github.com>
2021-08-04 21:31:35 +0800

    Update x86-64_open_frpc_part2.sh

--- x86-64_open_frpc_part2.sh
+++ x86-64_open_frpc_part2.sh
@@ -14,9 +14,9 @@
 #sed -i 's/192.168.1.1/192.168.1.254/g' package/base-files/files/bin/config_generate
 sed -i '/exit 0/i\uci set network.lan.ipaddr='192.168.1.254'\nuci set network.lan.proto='static'\nuci set network.lan.type='bridge'\nuci set network.lan.ifname='eth0'\nuci set network.lan.netmask='255.255.255.0'\nuci set network.lan.gateway='192.168.1.1'\nuci set network.lan.dns='192.168.1.1'\nuci commit network' package/lean/default-settings/files/zzz-default-settings
 
-# Modify default passwork:00000000
-sed -i -e 's#root::0:0:99999:7:::#root:$1$/YSxcdBO$bFuXE13KnaJb25YA8b6/1/:18825:0:99999:7:::#g' package/base-files/files/etc/shadow
-#sed -i -e 's!$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::!$1$/YSxcdBO$bFuXE13KnaJb25YA8b6/1/:18825:0:99999:7:::!g' package/lean/default-settings/files/zzz-default-settings
+# Modify default passwork:00000000/qazwsx939
+#sed -i -e 's#root::0:0:99999:7:::#root:$1$/YSxcdBO$bFuXE13KnaJb25YA8b6/1/:18825:0:99999:7:::#g' package/base-files/files/etc/shadow
+sed -i -e 's#root::0:0:99999:7:::#root:$1$aiWMDYOa$LUY5AmiiEv1XWE4m7PVG4.:18843:0:99999:7:::#g' package/base-files/files/etc/shadow
 
 # Download luci-app-Poweroff
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/uci-app-poweroff
