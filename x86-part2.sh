47ec0585a9a77db0825b023994f327d1b1aa1dd5
wzj939 <64715818+wzj939@users.noreply.github.com>
2021-08-04 16:30:48 +0800

    Update x86-part2.sh

--- x86-part2.sh
+++ x86-part2.sh
@@ -12,10 +12,10 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-sed -i '/exit 0/i\uci set network.lan.ipaddr='192.168.5.6'\nuci set network.lan.proto='static'\nuci set network.lan.type='bridge'\nuci set network.lan.ifname='eth0'\nuci set network.lan.netmask='255.255.255.0'\nuci set network.lan.gateway='192.168.5.1'\nuci set network.lan.dns='192.168.5.1'\nuci commit network' package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit 0/i\uci set network.lan.ipaddr='192.168.5.1'\nuci set network.lan.proto='static'\nuci set network.lan.type='bridge'\nuci set network.lan.ifname='eth0'\nuci set network.lan.netmask='255.255.255.0'\nuci set network.lan.gateway='192.168.5.1'\nuci set network.lan.dns='192.168.5.1'\nuci commit network' package/lean/default-settings/files/zzz-default-settings
 
 # Modify default passwork:00000000
-sed -i -e 's!root::0:0:99999:7:::!root:$1$/YSxcdBO$bFuXE13KnaJb25YA8b6/1/:18825:0:99999:7:::!g' package/base-files/files/etc/shadow
+sed -i -e 's#root::0:0:99999:7:::#root:$1$aiWMDYOa$LUY5AmiiEv1XWE4m7PVG4.:18843:0:99999:7:::#g' package/base-files/files/etc/shadow
 
 # Download luci-app-Poweroff
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/uci-app-poweroff
@@ -33,7 +33,7 @@ echo "config proxy
      option type 'http'
      option domain_type 'custom_domains'
      option custom_domains 'lede.venseco.ga'
-     option local_ip '192.168.5.6'
+     option local_ip '192.168.5.1'
      option local_port '80'
      option proxy_protocol_version 'disable'
      option use_encryption '1'
